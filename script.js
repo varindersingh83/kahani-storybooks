939b29c3c180f70ff5ea18790a0d32f1b0faf711
Varinder Singh <Varinder83singh@gmail.com>
2025-06-23 15:57:34 -0700

    Add storybook images and update website with .gitignore

--- script.js
+++ script.js
@@ -43,3 +43,30 @@ document.querySelectorAll(".section").forEach((section) => {
   section.style.transition = "opacity 0.6s ease, transform 0.6s ease";
   observer.observe(section);
 }); 
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("carousel-slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
